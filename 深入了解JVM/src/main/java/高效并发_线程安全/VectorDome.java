b95ff951554d664e20e33930f0e01bce06e522d9
LLS <2145604059@qq.com>
2019-01-13 19:57:15 +0800

    2019/1/13

---
+++ 深入了解JVM/src/main/java/高效并发_线程安全/VectorDome.java
@@ -0,0 +1,16 @@
+package 高效并发_线程安全;
+
+import java.util.Vector;
+
+public class VectorDome {
+    public static void main(String[] args) {
+        /*
+        * 学习JDK中Vector类
+        * 这是一个集合类，继承AbstractList类
+        * 但是这个类中暴露在外的方法都加上了synchronized关键字
+        * 可以在一定程度上可以说是线程安全的
+        * 写一个例子来学习Vector，测试Vector是否是线程安全的
+        * */
+
+    }
+}
